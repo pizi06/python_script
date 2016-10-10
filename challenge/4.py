c5d4d1c9e400b803723865f42b87d2cbcb12c18f
pizi06 <y-272@163.com>
2016-10-10 19:22:47 +0800

    3：为正则表达式的练习，4：还没发现规律，先提交记录一下规律

---
+++ challenge/4.py
@@ -0,0 +1,15 @@
+# -*- coding:utf8 -*-
+
+
+# http://www.pythonchallenge.com/pc/def/linkedlist.php
+# title: follow the chain  
+
+if __name__ == "__main__":
+    # http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345
+    # and the next nothing is 44827
+    # and the next nothing is 45439 
+    # Your hands are getting tired and the next nothing is 94485 72198
+    
+
+
+
